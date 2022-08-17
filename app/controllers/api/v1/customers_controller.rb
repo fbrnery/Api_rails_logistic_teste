21e221475d4051aa7f2998e4785dc93522c17b3e
Fabricio Miranda <fbrnery@gmail.com>
2022-08-17 11:35:07 -0300

    Customer Controller (Index)-cont

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,29 +1,70 @@
 class Api::V1::CustomersController < ApplicationController
-
   before_action :set_customer, only: [:show, :update, :destroy]
 
   # GET /customers
   def index
-    @customers = Customer.all
+    per_page = params[:per_page] ? params[:per_page] : 10
+    @customers = Customer.all.paginate(page: params[:page], per_page: per_page)
 
-    render json: @customers
+    if @customers.length >= 1
+      render json: {
+        status: 'SUCCESS',
+        message: 'Successfully loaded',
+        data: @customers,
+        per_page: per_page.to_i,
+        total_data: @customers.count,
+        current_page: params[:page].to_i ? params[:page].to_i : 0,
+        total_pages: @customers.total_pages
+      },
+      include: [
+        {facilities: {except: [:created_at, :updated_at]}},
+        {operations: {except: [:created_at, :updated_at]}},
+        {contacts: {except: [:created_at, :updated_at]}},
+      ],
+      :except => :operation_ids
+    else 
+      per_page = 0
+      total_pages = 0
+      render json: {
+        status: 'SUCCESS',
+        message: 'There are no customers registered on this page',
+        data: [],
+        per_page: per_page,
+        total_data: @customers.count,
+        current_page: params[:page].to_i ? params[:page].to_i : 0,
+        total_pages: @customers.total_pages
+      }
+    end
   end
 
   # GET /customers/1
   def show
-    render json: @customer
+    render json: {
+      status: 'SUCCESS',
+      message: 'Successfully loaded',
+      data: @customer
+    },
+    include: [
+      {facilities: {except: [:created_at, :updated_at]}},
+      {operations: {except: [:created_at, :updated_at]}},
+      {contacts: {except: [:created_at, :updated_at]}}
+    ]
   end
 
   # POST /customers
   def create
     @customer = Customer.new(customer_params)
-    
-      if @customer.save
-        render json: @customer, status: :created,
-      include: [{
-        operations: {except: [:created_at, :updated_at]}}
-      ]
 
+    if @customer.save
+      render json: {
+        status: 'SUCCESS',
+        message: 'Saved successfully',
+        data: @customer,
+        location: api_v1_customer_url(@customer)
+      },
+      include: [
+        {operations: {except: [:created_at, :updated_at]}}
+      ]
     else
       render json: @customer.errors, status: :unprocessable_entity
     end
@@ -33,8 +74,8 @@ class Api::V1::CustomersController < ApplicationController
   def update
     if @customer.update(customer_params)
       render json: @customer,
-      include: [{
-        operations: {except: [:created_at, :updated_at]}}
+      include: [
+        {operations: {except: [:created_at, :updated_at]}}
       ]
     else
       render json: @customer.errors, status: :unprocessable_entity
@@ -56,14 +97,14 @@ class Api::V1::CustomersController < ApplicationController
     def customer_params
       params.require(:customer)
       .permit(
-        :fantasyName, 
-        :custumerName, 
-        :taxid, 
-        :status, 
+        :fantasyName,
+        :customerName,
+        :taxId,
+        :status,
         :stateRegistration,
         :facility_id,
         :contact_id,
         operation_ids: []
-        )
+      )
     end
 end
